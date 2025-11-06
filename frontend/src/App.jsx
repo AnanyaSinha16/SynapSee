f449362b9ecea1948a81129c6a5268c990e1ea51
AnanyaSinha16 <2200033092@kluniversity.in>
2025-11-07 00:24:19 +0530

    Prepare frontend for Vercel deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 import Login from "./pages/Login";
-import About from "./pages/About";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import Navbar from "./components/Navbar";
 
-const PageTransition = ({ children }) => (
+const PageWrapper = ({ children }) => (
   <motion.div
     initial={{ opacity: 0, y: 20 }}
     animate={{ opacity: 1, y: 0 }}
@@ -21,22 +21,20 @@ const AnimatedRoutes = () => {
   const location = useLocation();
   return (
     <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<PageTransition><Login /></PageTransition>} />
-        <Route path="/about" element={<PageTransition><About /></PageTransition>} />
-        <Route path="/home" element={<PageTransition><Home /></PageTransition>} />
+      <Routes key={location.pathname} location={location}>
+        <Route path="/" element={<PageWrapper><Login /></PageWrapper>} />
+        <Route path="/home" element={<PageWrapper><Home /></PageWrapper>} />
+        <Route path="/about" element={<PageWrapper><About /></PageWrapper>} />
       </Routes>
     </AnimatePresence>
   );
 };
 
-const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <AnimatedRoutes />
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <Navbar />
+    <AnimatedRoutes />
+  </Router>
+);
 
 export default App;
