b7ddb91220acf6bc0c2e70456a5e31fa84786823
AnanyaSinha16 <2200033092@kluniversity.in>
2025-12-18 23:53:09 +0530

    App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,26 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import FloatingNav from "./components/FloatingNav";
-import Footer from "./components/Footer";
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import OcrDashboard from "./pages/OcrDashboard";
 
 function App() {
   return (
     <Router>
-      <FloatingNav />
-
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* Default route goes to Home */}
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+        <Route path="/ocr-dashboard" element={<OcrDashboard />} />
       </Routes>
-
-      <Footer />
     </Router>
   );
 }
